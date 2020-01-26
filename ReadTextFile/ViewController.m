56c83dbd2bf7c67ba9ec3f1bc46252538a24d869
John Tran <johnleetran@yahoo.com>
2020-01-26 01:36:41 -0800

    done

--- ReadTextFile/ViewController.m
+++ ReadTextFile/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *browseFileButton;
+@property (weak, nonatomic) IBOutlet UITextView *textReadFileView;
+@property (weak, nonatomic) IBOutlet UIImageView *imageFileView;
 
 @end
 
@@ -19,5 +22,28 @@
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)tapBrowseFileButton:(id)sender {
+    UIDocumentPickerViewController *documentPicker = [[UIDocumentPickerViewController alloc] initWithDocumentTypes:@[@"public.image", @"public.text", @"public.plain-text", @"text/plain"] inMode:UIDocumentPickerModeImport];
+    documentPicker.delegate = self;
+    documentPicker.allowsMultipleSelection = false;
+    [self presentViewController:documentPicker animated:YES completion:nil];
+}
+
+- (void)documentPicker:(UIDocumentPickerViewController *)controller didPickDocumentAtURL:(NSURL *)url {
+    if (controller.documentPickerMode == UIDocumentPickerModeImport) {
+        NSString* filePath = [url relativePath];
+        NSString* fileExt = [url pathExtension];
+
+        if([fileExt isEqualToString:@"txt"] ||  [fileExt isEqualToString:@"json"]){
+            NSString *content = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
+            [ _textReadFileView setText: content];
+        }else if([fileExt isEqualToString:@"jpg"] || [fileExt isEqualToString:@"jpeg"] || [fileExt isEqualToString:@"png"]){
+            NSString *ImageURL = filePath;
+            NSData *imageData = [NSData dataWithContentsOfURL:url];
+            _imageFileView.image = [UIImage imageWithData:imageData];
+        }
+
+    }
+}
 
 @end
